e244ed647ac6d3219f93b4bbe15fb5fab2cab05c
AndreyMpr <andreympr@gmail.com>
2025-03-15 11:37:03 +0900

    Add imports, main function wrapper and error handling.
    Create function wrapper, exceptions and add imports to work with excel files.

--- kessan_shorui_import.py
+++ kessan_shorui_import.py
@@ -1,1 +1,15 @@
+import pandas as pd
+from openpyxl import load_workbook
 
+def transfer_data():
+    try: ""
+         
+
+
+    except FileNotFoundError:
+        print("Error: One or more Excel files not found. Make sure all files are in the same directory as this script.\nエラー：1 つまたは複数の Excel ファイルが見つからない。すべてのファイルがこのスクリプトと同じフォルダーにあることを確認してください。")
+    except Exception as e:
+        print(f"An error occured: {str(e)}")
+
+if __name__ == "__main__":
+    transfer_data()
